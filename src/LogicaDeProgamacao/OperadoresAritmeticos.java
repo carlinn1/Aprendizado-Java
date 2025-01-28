988cf046f2aab6401bd650bbead3045fa3a6003a
carlinn1 <carlos_-425@hotmail.com>
2025-01-28 10:31:11 -0300

    OperadoresAritmeticos e Strings

--- src/LogicaDeProgamacao/OperadoresAritmeticos.java
+++ src/LogicaDeProgamacao/OperadoresAritmeticos.java
@@ -3,7 +3,22 @@ package LogicaDeProgamacao;
 public class OperadoresAritmeticos {
     public static void main(String[] args) {
 
+        /*
+            Adição ( + )
+            subtração ( - )
+            Multiplicação ( * )
+            Divisão ( / )
+            Modulo ( % ) = é o resto da divisão
+        */
 
+
+            double pao = 1.50;
+            double queijo = 2.30;
+            double cebola = 0.75;
+
+            double total = pao + queijo + cebola;
+
+            System.out.println("Total: " + total);
 
 
     }
---
+++ src/LogicaDeProgamacao/Strings.java
@@ -0,0 +1,15 @@
+package LogicaDeProgamacao;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        //Strings são sequencias de palavras diferente de CHAR.
+        //Isso se chama concatenação de Strings ou seja a junção de palavras!
+
+        String nome = "Carlos";
+        String sobrenome = "Henrique";
+
+        System.out.println(nome + " " + sobrenome);
+
+    }
+}
