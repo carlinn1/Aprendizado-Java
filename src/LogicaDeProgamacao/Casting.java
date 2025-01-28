10992a4d92f87efd7f7d94beea7eb1adec9a2df0
carlinn1 <carlos_-425@hotmail.com>
2025-01-28 11:01:20 -0300

    casting e Conversao de Tipos

--- src/LogicaDeProgamacao/Casting.java
+++ src/LogicaDeProgamacao/Casting.java
@@ -3,8 +3,24 @@ package LogicaDeProgamacao;
 public class Casting {
     public static void main(String[] args) {
 
+        //Conversao de Tipos
 
+        int numero = 10;
+        double d = numero;
 
+        double d2 = 10.5;
+        int num2 = (int) d2;
+
+        Integer num3 = 10;
+        String num4 = num3.toString();
+
+        String num5 = "10";
+        Integer num6 = Integer.parseInt(num5);
+
+        System.out.println("Int para double: " + num2);
+        System.out.println("Double para int: " + d);
+        System.out.println("Integer para String: " + num4);
+        System.out.println("String para Integer: " + num6);
 
     }
 }
