65d8913c1fdfeeda9734279a666cde05b1258622
carlinn1 <carlos_-425@hotmail.com>
2025-01-28 10:03:04 -0300

    Tipos Primitivos

---
+++ src/LógicaDeProgamação/HelloWord.java
@@ -0,0 +1,13 @@
+package LógicaDeProgamação;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello Word!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ src/LógicaDeProgamação/TiposPrimitivos.java
@@ -0,0 +1,44 @@
+package LógicaDeProgamação;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+     //Tipos Primitivos em Java
+        //Existem 8 tipos primitivos em java
+            //Inteiros
+
+                byte b = 127;
+                short s = 32767;
+                int i = 2147483647;
+                long l = 9223372036854775807L;
+
+             //Double e Float
+
+                double d = 3.14159265358979323846;
+                float f = 3.14159265358979323846f;
+
+             //Caractere e Booleano
+
+                char c = 'A';
+                boolean bo = true;
+
+                System.out.println("---------------------------------------");
+
+                System.out.println("Byte:" + b);
+                System.out.println("Short:" + s);
+                System.out.println("Int:" + i);
+                System.out.println("Long:" + l);
+
+                System.out.println("---------------------------------------");
+
+                System.out.println("Double:" + d);
+                System.out.println("Float:" + f);
+
+                System.out.println("---------------------------------------");
+
+                System.out.println("Char:" + c);
+                System.out.println("Boolean:" + bo);
+
+                System.out.println("---------------------------------------");
+    }
+}
