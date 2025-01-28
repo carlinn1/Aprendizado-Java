3f811c9ed6af110db0f3a807562739169ea3c9a4
carlinn1 <carlos_-425@hotmail.com>
2025-01-28 14:26:12 -0300

    While, Do While, For estruturas de repetição

---
+++ src/LogicaDeProgamacao/EstruturasRepetitivas.java
@@ -0,0 +1,33 @@
+package LogicaDeProgamacao;
+
+public class EstruturasRepetitivas {
+    public static void main(String[] args) {
+
+        // While
+        // Do while
+        // For
+
+//        int contador = 0;
+//        while (contador < 10) {
+//
+//            System.out.println("contador = " + contador);
+//            contador++;
+//
+//        }
+
+        // Exemplo Do While
+
+//        int contador = 0;
+//        do {
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        } while (contador < 10);]
+
+        //Exemplo de For
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+
+    }
+}
