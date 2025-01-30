53a501fe54c00dd411dc2ae6a6655f89c6fc196f
carlinn1 <carlos_-425@hotmail.com>
2025-01-30 16:58:06 -0300

    ArraysList

---
+++ src/LogicaDeProgamacao/Arraylists.java
@@ -0,0 +1,26 @@
+package LogicaDeProgamacao;
+
+import java.util.ArrayList;
+
+public class Arraylists{
+    public static void main(String[] args) {
+
+        ArrayList<String> nomes = new ArrayList<>();
+
+        nomes.add("Carlos");
+        nomes.add("Pablo");
+        nomes.add("Eduardo");
+        nomes.add("Edna");
+
+//        System.out.println(nomes);
+
+        nomes.remove(0);
+
+
+        System.out.println(nomes.get(0));
+        System.out.println(nomes.get(1));
+        System.out.println(nomes.get(2));
+//        System.out.println(nomes.get(3));
+
+    }
+}
